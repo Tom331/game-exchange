b98dfee3f10590df2a9175f776e7ed3ad7218365
LAPTOP-B6J9N6OC\talai <t.alain@live.ca>
2021-03-14 01:07:29 -0800

    change game__c to account

--- blog/models.py
+++ blog/models.py
@@ -23,7 +23,7 @@ class Post(models.Model):
     def get_absolute_url(self):
         return reverse('post-detail', kwargs={'pk': self.pk})
 
-class Game__c(models.Model):
+class account(models.Model):
     name = models.TextField() # Unrestricted text
 
     def __str__(self):
