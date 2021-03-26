f63def76365a5dbaf714975304c677ace7b3d167
LAPTOP-B6J9N6OC\talai <t.alain@live.ca>
2021-03-26 01:04:38 -0700

    ~~~~~~~~~ WSS CHAT FULLY WORKING!!!!! :D ~~~~~~~~~

--- core/settings.py
+++ core/settings.py
@@ -15,7 +15,7 @@ import os
 import dj_database_url
 import pdb
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+# Build paths inside the project like this: BASE_DIR / 'subdir'. 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
